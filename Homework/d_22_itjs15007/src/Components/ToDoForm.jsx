4eb08b718eb1ecdc8eb3fb8592689ab7c1529048
predrag-ristic <ristic.s.predrag@gmail.com>
2023-02-26 23:14:36 +0100

    Homework 22 (working but not complete)

---
+++ Homework/d_22_itjs15007/src/Components/ToDoForm.jsx
@@ -0,0 +1,60 @@
+import React, { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
+
+const ToDoForm = () => {
+    const [newItem, setNewItem] = useState("");
+    const [items, setItems] = useState([]);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if (!newItem) {
+            alert("Add a new To Do item, don't be lazy!");
+            return;
+        }
+
+        const item = {
+            id: uuidv4(),
+            value: newItem,
+        };
+
+        setItems((oldList) => [...oldList, item]);
+
+        setNewItem("");
+    };
+
+    const deleteItem = (id) => {
+        const newArray = items.filter((item) => item.id !== id);
+        setItems(newArray);
+    };
+
+    return (
+        <div className="App">
+            <h1>To Do List</h1>
+
+            <form onSubmit={handleSubmit}>
+                <input
+                    type="text"
+                    placeholder="Add an item"
+                    value={newItem}
+                    onChange={(e) => setNewItem(e.target.value)}
+                />
+                <button type="submit">Add</button>
+            </form>
+
+            <ul>
+                {items.map((item) => {
+                    return (
+                        <li key={item.id}>
+                            {item.value}{" "}
+                            <button onClick={() => deleteItem(item.id)}>
+                                <span style={{ color: "red" }}>X</span>
+                            </button>
+                        </li>
+                    );
+                })}
+            </ul>
+        </div>
+    );
+}
+
+export default ToDoForm
